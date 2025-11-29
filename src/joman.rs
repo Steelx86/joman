c32b3338c8db26578c61bf9027d11e73fd20c3a0
dlambda <20195teel@gmail.com>
2025-11-28 23:53:12 -0500

    some work?

--- src/joman.rs
+++ src/joman.rs
@@ -1,1 +1,77 @@
+use core::panic;
+use std::fs;
 
+use crate::encryption::{aes_decrypt, aes_encrypt, aes_gen_key};
+
+const CONFIG: &str = "[settings]\n key = ";
+
+pub fn initialize() {
+    fs::create_dir_all(".joman").expect("Failed to create journal directory");
+    fs::write(".joman/config.toml", CONFIG).expect("failed to write config.toml");
+}
+
+fn file_to_string(file_path: &str) -> String {
+    fs::read_to_string(file_path).expect("Failed to read file")
+}
+
+fn encrypt_string(plaintext: &str, key: &str) -> String {
+    aes_encrypt(plaintext, key).expect("Encryption failed")
+}
+
+fn write_to_file(file_path: &str, data: &str) {
+    fs::write(file_path, data).expect("Failed to write to file");
+}
+
+pub fn add_file(file_path: &str, key: Option<&str>) {
+    let plaintext_str = file_to_string(file_path);
+
+    let encrypted_str = match key {
+        Some(k) => encrypt_string(&plaintext_str, k),
+        None => panic!("No key provided for encryption"),
+    };
+
+    write_to_file(file_path, &encrypted_str);
+}
+
+fn file_to_bytes(file_path: &str) -> Vec<u8> {
+    fs::read(file_path).expect("Failed to read file")
+}
+
+fn bytes_to_str(data: &[u8]) -> &str {
+    std::str::from_utf8(data).expect("Failed to convert bytes to string")
+}
+
+pub fn read_file(file_path: &str, key: Option<&str>) -> String {
+    let data = fs::read(file_path).expect("Failed to read entry file");
+
+    let encrypted_str = std::str::from_utf8(&data).expect("Failed to convert entry data to string");
+
+    let plaintext_str = match key {
+        Some(k) => aes_decrypt(encrypted_str, k).expect("Decryption failed"),
+        None => encrypted_str.to_string(),
+    };
+
+    plaintext_str
+}
+
+pub fn lock_directory(key: Option<&str>) {
+    let aes_key = match key {
+        Some(k) => k.to_string(),
+        None => aes_gen_key(),
+    };
+
+    let entries = fs::read_dir(".joman").expect("Failed to read journal directory");
+
+    for entry in entries {
+        let entry = entry.expect("Failed to read directory entry");
+        let path = entry.path();
+        if path.is_file() {
+            let data = fs::read_to_string(&path).expect("Failed to read entry file");
+            let encrypted_data =
+                aes_encrypt(&data, &aes_key).expect("Failed to encrypt entry data");
+            fs::write(&path, encrypted_data).expect("Failed to write encrypted data to file");
+        }
+    }
+
+    println!("Directory locked. AES-256 Key: {}", aes_key);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::{Arg, ArgMatches, Command, Parser};
 
-use crate::encryption::aes_gen_key;
+use crate::{encryption::aes_gen_key};
 
-mod joman;
 mod encryption;
+mod joman;
 
 fn cli() -> Command {
     clap::Command::new("joman")
@@ -13,32 +13,22 @@ fn cli() -> Command {
         .arg_required_else_help(true)
         .subcommand(
             Command::new("init")
-                .about("initializes a journal system")
+                .about("initializes an encrypted journal inside the current directory"),
         )
         .subcommand(
             Command::new("add")
                 .about("adds a new entry to directory")
-                .arg(
-                    Arg::new("directory`")
-                        .help("Path of the target directory")
-                        .required(true)
-                        .value_name("DIR")
-                )
                 .arg(
                     Arg::new("file")
                         .help("Path of the entry file to add")
                         .required(true)
                         .value_name("FILE"),
                 ),
-                
         )
         .subcommand(
             Command::new("read")
                 .about("read an entry in the directory")
-                .arg(
-                    Arg::new("file")
-                        .help("name of the entry file")
-                )
+                .arg(Arg::new("file").help("name of the entry file")),
         )
         .subcommand(
             Command::new("lock")
@@ -48,32 +38,34 @@ fn cli() -> Command {
                         .required(true)
                         .value_name("DIR"),
                 )
-                .arg(
-                    Arg::new("key")
-                        .help("AES-256 key")
-                        .value_name("KEY"),
-                )
+                .arg(Arg::new("key").help("AES-256 key").value_name("KEY"))
                 .about("encrypts a directory of entries and returns the key"),
         )
-        .subcommand(
-            Command::new("generate")
-                .about("generates a AES-256 key")
-        )
+        .subcommand(Command::new("generate").about("generates a AES-256 key"))
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = cli().get_matches();
 
     match matches.subcommand() {
-        Some(("init", sub_matches)) => {}
-        Some(("add", sub_matches)) => {}
+        Some(("init", sub_matches)) => {
+            joman::initialize();
+        }
+        Some(("add", sub_matches)) => {
+            let file_path = sub_matches
+                .get_one::<String>("file")
+                .expect("file argument missing");
+
+            joman::add_file(file_path, None);
+        }
         Some(("read", sub_matches)) => {}
         Some(("lock", sub_matches)) => {}
         Some(("generate", sub_matches)) => {
             let key = aes_gen_key();
+
             println!("generated key: {}", key)
         }
-        _ => !unreachable!(),
+        _ => unreachable!(),
     }
 
     Ok(())
